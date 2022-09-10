33f2455ee2e56035faab187e5b3b6580644e1289
Pau Cardone <96506186+mutatiismutandiis@users.noreply.github.com>
2022-09-10 12:18:13 +0200

    Connected with FrontEnd

--- portfoliocardone/src/main/java/com/portfoliocardone/portfoliocardone/Controller/EducationController.java
+++ portfoliocardone/src/main/java/com/portfoliocardone/portfoliocardone/Controller/EducationController.java
@@ -4,6 +4,7 @@ import com.portfoliocardone.portfoliocardone.Entity.Education;
 import com.portfoliocardone.portfoliocardone.Interface.IEducationService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class EducationController {
     
     @Autowired IEducationService ieducationService;
@@ -51,5 +53,10 @@ public class EducationController {
     
         ieducationService.saveEducation(education);
         return education;
+    }
+    
+    @GetMapping("/education/find/profile")
+    public Education findEducation(){
+        return ieducationService.findEducation((long)1);
     }
 }
--- portfoliocardone/src/main/java/com/portfoliocardone/portfoliocardone/Controller/PersonController.java
+++ portfoliocardone/src/main/java/com/portfoliocardone/portfoliocardone/Controller/PersonController.java
@@ -4,6 +4,7 @@ import com.portfoliocardone.portfoliocardone.Entity.Person;
 import com.portfoliocardone.portfoliocardone.Interface.IPersonService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonController {
     
     @Autowired IPersonService ipersonService;
@@ -51,5 +53,10 @@ public class PersonController {
     
         ipersonService.savePerson(person);
         return person;
-    } 
+    }
+    
+    @GetMapping("/person/find/profile")
+    public Person findPerson(){
+        return ipersonService.findPerson((long)1);
+    }
 }
